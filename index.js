2b8ac96a7a5284145c82a52885d549b004f7f2b3
Romain Dodet <romain.dodet00@gmail.com>
2022-11-19 10:17:32 +0100

    Create GAN program
    Signed-off-by: Romain Dodet <romain.dodet00@gmail.com>

---
+++ index.js
@@ -0,0 +1,79 @@
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+const path = require("path");
+
+const url = process.argv[2];
+
+if (!url) {
+  console.log(`Please enter a URL (e.g. "npm start https://rarible.com/boredapeyachtclub").`);
+  return;
+}
+
+(async () => {
+  console.log("Loading...");
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+
+  await page.goto(url);
+  await page.setViewport({
+    width: 1200,
+    height: 800,
+  });
+
+  await page.waitForSelector(".ReactVirtualized__Grid");
+
+  const pageTitle = await page.title();
+  const collection = await pageTitle.split("-").shift().trim();
+  let dir = collection.replace(/[^a-z0-9]/gi, "_").toLowerCase();
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
+
+  let currentImage = 1;
+
+  page.on("response", async (response) => {
+    const imageUrl = response.url();
+    if (response.request().resourceType() === "image") {
+      response.buffer().then((file) => {
+        if (imageUrl.includes("t_image_preview")) {
+          const fileName = imageUrl.split("/").pop() + ".avif";
+          const filePath = path.resolve(__dirname, dir, fileName);
+          const writeStream = fs.createWriteStream(filePath);
+          writeStream.write(file);
+          console.log(`${collection} #${currentImage} saved to ${dir}/${fileName}`);
+          currentImage++;
+        }
+      });
+    }
+  });
+
+  await autoScroll(page);
+
+  await page.evaluate(() => {
+    const elements = [...document.querySelectorAll("button")];
+    const targetElement = elements.find((e) => e.innerText.includes("Load more"));
+    targetElement && targetElement.click();
+  });
+
+  await autoScroll(page);
+  await browser.close();
+})();
+
+async function autoScroll(page) {
+  await page.evaluate(async () => {
+    await new Promise((resolve) => {
+      let totalHeight = 0;
+      let distance = 500;
+      let timer = setInterval(() => {
+        let scrollHeight = document.body.scrollHeight;
+        window.scrollBy(0, distance);
+        totalHeight += distance;
+
+        if (totalHeight >= scrollHeight) {
+          clearInterval(timer);
+          resolve();
+        }
+      }, 1000);
+    });
+  });
+}
